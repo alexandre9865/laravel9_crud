740d67be7747787b8efa3a7fc0208d5b9e6a8dec
alexandre9865 <alexandremontebelo@yahoo.com.br>
2023-03-14 20:46:01 -0300

    Profile API with validation and basic crud working

---
+++ app/Http/Controllers/Api/ProfileController.php
@@ -0,0 +1,94 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\Profile;
+use App\Http\Requests\ProfileRequest;
+use App\Http\Resources\ProfileResource;
+use Illuminate\Http\Request;
+
+class ProfileController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $profiles = Profile::all();
+        return ProfileResource::collection($profiles);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  App\Http\Requests\ProfileRequest  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(ProfileRequest $request)
+    {
+        $validated = $request->validated();
+        $profiles = Profile::create($validated);
+    
+        return new ProfileResource($profiles);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Profile  $profile
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Profile $profile)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Profile  $profile
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Profile $profile)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  App\Http\Requests\ProfileRequest  $request
+     * @param  \App\Models\Profile  $profile
+     * @return \Illuminate\Http\Response
+     */
+    public function update(ProfileRequest $request, Profile $profile)
+    {
+        $profile->update($request->all());
+        return new ProfileResource($profile);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Profile  $profile
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Profile $profile)
+    {
+        $profile->delete();
+        return response(null, 204);
+    }
+}
---
+++ app/Http/Requests/ProfileRequest.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Contracts\Validation\Validator;
+use Illuminate\Validation\ValidationException;
+use Illuminate\Http\Exceptions\HttpResponseException;
+
+class ProfileRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, mixed>
+     */
+    public function rules()
+    {
+        return [
+            'first_name'    => ['required', 'max:65'],
+            'last_name'     => ['required', 'max:65'],
+            'dbo'           => ['required', 'before_or_equal:'.Date('Y-m-d')],
+            'gender'        => ['required', 'integer', 'between:0,2']
+        ];
+    }
+
+    protected function failedValidation(Validator $validator)
+    {
+        if ($this->expectsJson()) {
+            $errors = (new ValidationException($validator))->errors();
+            throw new HttpResponseException(
+                response()->json(['data' => null, 'error' => $errors], 422)
+            );
+        }
+
+        parent::failedValidation($validator);
+    }
+
+    /**
+     * Get the error messages for the defined validation rules.
+     *
+     * @return array<string, string>
+     */
+    public function messages(): array
+    {
+        return [
+            'first_name.required'   => 'The first name field is required as you can see.',
+            'first_name.max'        => 'The first name field cannot be greater than 65 characters.',
+            'last_name.required'    => 'The last name field is required as you can see.',
+            'last_name.max'         => 'The last name field cannot be greater than 65 characters.',
+            'dbo.required'          => 'The date of birth field is required.',
+            'dbo.before_or_equal'   => 'The date of birth cannot be greater than the current date.',
+            'gender.required'       => 'The gender field is required.',
+            'gender.between'        => 'Valid values for gender: 0 (Male), 1 (Female), 2 (Other).',
+            'gender.integer'        => 'The gender field accepts only integer numbers.',
+        ];
+    }
+}

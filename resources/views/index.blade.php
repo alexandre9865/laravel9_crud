3168d418a587282f335f7125b8df113c0b9a8bce
alexandre9865 <alexandremontebelo@yahoo.com.br>
2023-03-18 18:33:04 -0300

    Initial page

---
+++ resources/views/index.blade.php
@@ -0,0 +1,12 @@
+@extends('layout')
+
+@section('content')
+<div class="row">
+    <div class="col-lg-12 margin-tb">
+        <div class="mt-5 text-center">
+            <h2>Laravel 9 CRUD</h2>
+            <h6>Developed by Alexandre M Montebelo</h6>
+        </div>
+    </div>
+</div>
+@endsection

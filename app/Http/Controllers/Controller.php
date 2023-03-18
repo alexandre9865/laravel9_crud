0b19f7397296199dc8507928740f605df1781932
alexandre9865 <alexandremontebelo@yahoo.com.br>
2023-03-18 17:58:45 -0300

    Controller function to process requests from internal API

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -6,8 +6,18 @@ use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
 
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+
+    public function internalRequest($route, $method, $data=[])
+    {
+        $request = Request::create($route, $method, $data);
+        $request->headers->set('Accept', 'application/json');
+        $response = Route::dispatch($request);
+        return json_decode($response->getContent());
+    }
 }

071088a461af0ab42b2012fe8966321a1081da3b
alexandre9865 <alexandremontebelo@yahoo.com.br>
2023-03-19 00:50:06 -0300

    Report frontend CRUD files structure (Not yet correctly implemented)

---
+++ resources/views/report/edit.blade.php
@@ -0,0 +1,72 @@
+@extends('layout')
+@section('title', 'Edit Profile')
+@section('content')
+<div class="row">
+    <div class="col-lg-12 margin-tb">
+        <br>
+        <div class="float-left">
+            <h2>Edit Profile</h2>
+        </div>
+        <div class="float-right">
+            <a class="btn btn-primary" href="{{ route('profiles.index') }}" enctype="multipart/form-data">
+                Back</a>
+        </div>
+    </div>
+</div>
+@if(session('status'))
+<div class="alert alert-success mb-1 mt-1">
+    {{ session('status') }}
+</div>
+@endif
+<br>
+<form action="{{ route('profiles.update',$profile->id_profile) }}" method="POST" enctype="multipart/form-data">
+    @csrf
+    @method('PUT')
+    <div class="row">
+        <div class="col-xs-12 col-sm-12 col-md-12">
+            <div class="form-group">
+                <strong>First Name:</strong>
+                <input type="text" name="first_name" value="{{ $profile->first_name }}" class="form-control"
+                    placeholder="First Name">
+                @error('first_name')
+                <div class="alert alert-danger mt-1 mb-1">{{ $message }}</div>
+                @enderror
+            </div>
+        </div>
+        <div class="col-xs-12 col-sm-12 col-md-12">
+            <div class="form-group">
+                <strong>Last Name:</strong>
+                <input type="text" name="last_name" class="form-control" placeholder="Last Name"
+                    value="{{ $profile->last_name }}">
+                @error('last_name')
+                <div class="alert alert-danger mt-1 mb-1">{{ $message }}</div>
+                @enderror
+            </div>
+        </div>
+        <div class="col-xs-12 col-sm-12 col-md-12">
+            <div class="form-group">
+                <strong>Date Of Birth:</strong>
+                <input type="date" name="dbo" value="{{ $profile->dbo }}" class="form-control"
+                    placeholder="Date Of Birth">
+                @error('dbo')
+                <div class="alert alert-danger mt-1 mb-1">{{ $message }}</div>
+                @enderror
+            </div>
+        </div>
+        <div class="col-xs-12 col-sm-12 col-md-12">
+            <div class="form-group">
+                <strong>Gender:</strong>
+                <select name="gender" class="form-control">
+                    <option value="0" {{ $profile->gender == '0' ? 'selected' : '' }}>Male</option>
+                    <option value="1" {{ $profile->gender == '1' ? 'selected' : '' }}>Female</option>
+                    <option value="2" {{ $profile->gender == '2' ? 'selected' : '' }}>Other</option>
+                </select>
+                @error('gender')
+                <div class="alert alert-danger mt-1 mb-1">{{ $message }}</div>
+                @enderror
+            </div>
+        </div>
+        <button type="submit" class="btn btn-primary ml-3">Submit</button>
+    </div>
+</form>
+@endsection
---
+++ resources/views/report/index.blade.php
@@ -0,0 +1,56 @@
+@extends('layout')
+
+@section('title', 'Profile List')
+@section('content')
+<div class="row">
+    <div class="col-lg-12 margin-tb">
+        <br>
+        <div class="float-left">
+            <h2>Profile List</h2>
+        </div>
+        <div class="float-right mb-2">
+            <a class="btn btn-success" href="{{ route('profiles.create') }}"> Create Profile</a>
+        </div>
+    </div>
+</div>
+@if ($message = Session::get('success'))
+    <div class="alert alert-success">
+        <p>{{ $message }}</p>
+    </div>
+@endif
+@if ($message = Session::get('error'))
+    <div class="alert alert-danger">
+        <p>{{ $message }}</p>
+    </div>
+@endif
+<br>
+<table class="table table-bordered">
+    <thead>
+        <tr>
+            <th>First Name</th>
+            <th>Last Name</th>
+            <th>Gender</th>
+            <th>Date Of Birth</th>
+            <th width="230px">Action</th>
+        </tr>
+    </thead>
+    <tbody>
+        @foreach ($profiles as $profile)
+            <tr>
+                <td>{{ $profile->first_name }}</td>
+                <td>{{ $profile->last_name }}</td>
+                <td>{{ $profile->gender_name }}</td>
+                <td>{{ $profile->dbo }}</td>
+                <td>
+                    <form action="{{ route('profiles.destroy',$profile->id_profile) }}" method="Post">
+                        <a class="btn btn-primary" href="{{ route('profiles.edit',$profile->id_profile) }}">Edit</a>
+                        @csrf
+                        @method('DELETE')
+                        <button type="submit" class="btn btn-danger">Delete</button>
+                    </form>
+                </td>
+            </tr>
+            @endforeach
+    </tbody>
+</table>
+@endsection

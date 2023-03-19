57406b9aff70ea4adc8670ff5e75409192605945
alexandre9865 <alexandremontebelo@yahoo.com.br>
2023-03-19 12:47:03 -0300

    Mix npm install and config jquery and jquery-ui. Created specifics js files to each module. Created Datepicker to dbo field.

---
+++ public/js/profile.js
@@ -0,0 +1,9 @@
+/******/ (() => { // webpackBootstrap
+var __webpack_exports__ = {};
+/*!********************************************!*\
+  !*** ./resources/views/profile/profile.js ***!
+  \********************************************/
+console.log($('.btn'));
+$("#dbo").datepicker();
+/******/ })()
+;
--- webpack.mix.js
+++ webpack.mix.js
@@ -1,4 +1,7 @@
 let mix = require('laravel-mix');
 
 mix.js(['resources/js/app.js', 'resources/js/bootstrap.js'], 'public/js')
-    .postCss('resources/css/app.css', 'public/css', [])
+    .postCss('resources/css/app.css', 'public/css', []);
+
+mix.js(['resources/views/report/report.js'], 'public/js');
+mix.js(['resources/views/profile/profile.js'], 'public/js');

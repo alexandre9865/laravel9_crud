c631dee626df87ed1bcc950456955722c88d1ca2
alexandre9865 <alexandremontebelo@yahoo.com.br>
2023-03-18 22:55:49 -0300

    removed profile id column from list table

--- resources/views/profile/index.blade.php
+++ resources/views/profile/index.blade.php
@@ -22,7 +22,6 @@
 <table class="table table-bordered">
     <thead>
         <tr>
-            <th>Profile ID</th>
             <th>First Name</th>
             <th>Last Name</th>
             <th>Gender</th>
@@ -33,7 +32,6 @@
     <tbody>
         @foreach ($profiles as $profile)
             <tr>
-                <td>{{ $profile->id_profile }}</td>
                 <td>{{ $profile->first_name }}</td>
                 <td>{{ $profile->last_name }}</td>
                 <td>{{ $profile->gender_name }}</td>

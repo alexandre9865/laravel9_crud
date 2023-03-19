048b6296eac561f4751f02cce5aee8f5e197e447
alexandre9865 <alexandremontebelo@yahoo.com.br>
2023-03-18 21:47:14 -0300

    added js file to base layout

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -1,18 +1,19 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <title>@yield('title')</title>
-    <link rel="stylesheet" href="{{ mix('css/app.css') }}">
+    <head>
+        <meta charset="UTF-8">
+        <title>@yield('title')</title>
+        <link rel="stylesheet" href="{{ mix('css/app.css') }}">
 
-</head>
-<body>
-    <header>
-        @include('menu.header')
-    </header>
-    <section class="container mt-2">
-        @yield('content')
-    </section>
-   
-</body>
+    </head>
+    <body>
+        <header>
+            @include('menu.header')
+        </header>
+        <section class="container mt-2">
+            @yield('content')
+        </section>
+    
+    </body>
+    <script src="{{ mix('js/app.js')}}"></script>
 </html>

70ef9cc9dac0472b8c7444d2458c43041f2693d6
alexandre9865 <alexandremontebelo@yahoo.com.br>
2023-03-19 13:26:05 -0300

    Fix dbo format on edit.blade from Profile

--- resources/views/profile/edit.blade.php
+++ resources/views/profile/edit.blade.php
@@ -46,7 +46,7 @@
         <div class="col-xs-12 col-sm-12 col-md-12">
             <div class="form-group">
                 <strong>Date Of Birth:</strong>
-                <input type="text" name="dbo" id="dbo" value="{{ $profile->dbo }}" class="form-control"
+                <input type="text" name="dbo" id="dbo" value="{{ $profile->dbo->format('d/m/Y') }}" class="form-control"
                     placeholder="Date Of Birth">
                 @error('dbo')
                 <div class="alert alert-danger mt-1 mb-1">{{ $message }}</div>
